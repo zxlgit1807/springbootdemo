cc518fa480ee035d065e614bf2a73f1f1695627a
zxlgit1807 <zxcvbn719>
2018-09-06 00:00:08 +0800

    添加性别枚举

--- src/main/resources/db/V1_0_0__init.sql
+++ src/main/resources/db/V1_0_0__init.sql
@@ -4,6 +4,7 @@ CREATE TABLE zxl_user (
   user_name VARCHAR(64) NOT NULL COMMENT '姓名',
   login_name VARCHAR(16) NOT NULL COMMENT '登录名',
   loign_pwd VARCHAR(32) NOT NULL COMMENT '登录密码',
+  sex INT NOT NULL COMMENT '性别',
   birth_day DATE COMMENT '出生日期',
   create_time DATETIME NOT NULL COMMENT '创建时间',
   update_time TIMESTAMP NOT NULL COMMENT '修改时间',
